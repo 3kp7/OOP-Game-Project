562e8417302ecd82b8ab5885cd0db42dea7c4221
3kp7 <86517323+3kp7@users.noreply.github.com>
2023-01-02 01:24:54 +0300

    First Game

---
+++ Game1/Game1/Settings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    };
+
+
+        public class Settings
+        {
+            public static int Width { get; set; }
+            public static int Height { get; set; }
+            public static int Speed { get; set; }
+            public static int Score { get; set; }
+            public static int Points { get; set; }
+            public static bool GameOver { get; set; }
+            public static Direction direction { get; set; }
+
+            public Settings()
+            {
+                Width = 14;
+                Height = 14;
+                Speed = 12;
+                Score = 0;
+                Points = 100;
+                GameOver = false;
+                direction = Direction.Down;
+            }
+        }
+  
+}
